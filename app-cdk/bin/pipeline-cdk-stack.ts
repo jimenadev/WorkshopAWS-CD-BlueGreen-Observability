512ccef0af54df63038d3423eab68c585e814156
Jimena Silva <dev.jimena.silva@gmail.com>
2024-08-09 14:20:35 -0400

    first commit

---
+++ app-cdk/bin/pipeline-cdk-stack.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+/*import * as cdk from 'aws-cdk-lib';
+import { PipelineCdkStack  } from '../lib/pipeline-cdk-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'pipeline-Workshop-CICD-AWS', {
+  env: {
+    account: '891377226170',
+    region: 'us-east-1',
+  }
+});
+
+app.synth();*/
