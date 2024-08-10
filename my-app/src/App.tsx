690d93397b8e5bd797a725d0c86ff1c0d1d26751
Jimena Silva <dev.jimena.silva@gmail.com>
2024-08-09 20:12:26 -0400

    Added Blue/Green Prod delivery

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p>CI/CD Test 3</p>
+      <p>This is green!</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
